38826abef4e40ec9b41b52c851a505a09e8d0f13
Irene201 <56350981+Irene201@users.noreply.github.com>
2021-10-01 21:56:05 +0200

    Add files via upload
    Ny

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-const input = document.querySelector('input');
-const text = input.value;
+function lagg() {
+    const input = document.querySelector('input');
 
-const list = document.querySelector('ul');
-const item = document.createElement('li');
-list.appendChild(item);
-const itemLabel = document.createElement('span');
-itemLabel.innerText=text;
-item.appendChild(itemLabel);
-if(text.length == 0) {
-    document.querySelector('small').innerText = 'Input must not be empty';
-    return;
-} else {
-    document.querySelector('small').innerText = '';
+    const text = input.value;
+    const label = document.querySelector('p');
+    let completedCount = 0;
+    
+    if(text.length == 0) {
+        document.querySelector('small').innerText = 'Input must not be empty';
+        return;
+    } else {
+        document.querySelector('small').innerText = '';
+        const list = document.querySelector('ul');
+        const item = document.createElement('li');
+        list.appendChild(item);
+
+        const itemLabel = document.createElement('span');
+        itemLabel.innerText = text;
+        item.appendChild(itemLabel);
+        input.value = '';
+        itemLabel.addEventListener('click', function() {
+            if(item.getAttribute('class') == 'completed') {
+                item.setAttribute('class', '');
+                completedCount--;
+            } else {
+                item.setAttribute('class', 'completed');
+                completedCount++;
+            }
+            
+        });
+        const trashcan = document.createElement('span');
+        trashcan.innerHTML = '&#x15F5D1;';
+        trashcan.setAttribute('class', 'trashcan');
+        item.appendChild(trashcan);
+        trashcan.addEventListener('click', function() {
+            item.remove();
+        });
+        label.innerText = `${completedCount} completed`;
+    }
+    
 }
-input.value = '';
-itemLabel.addEventListener('click', function() {
-    if(item.getAttribute('class') == 'completed') {
-        item.setAttribute('class', '');
-    } else {
-        item.setAttribute('class', 'completed');
-    }
-});
 
-const label = document.querySelector('p');
-let completedCount = 0;
-itemLabel.addEventListener('click', function() {
-    if(item.getAttribute('class') == 'completed') {
-        item.setAttribute('class', '');
-        completedCount--;
-    } else {
-        item.setAttribute('class','completed');
-        completedCount++;
-    }
-});
-
-label.innerText = '${completedCount} completed';
-const trashcan = document.createElement('span');
-trashcan.innerHTML = '&#x1F5D1;';
-trashcan.setAttribute('class','trashcan');
-item.appendChild(trashcan);
-trashcan.addEventListener('click', function() {
-    item.remove();
-});
+const button = document.querySelector('button');
+button.addEventListener("click", lagg, false);
